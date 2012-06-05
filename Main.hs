c25c27d59d02d06f293cf35ecf049d2a7377faf0
Michael Xavier <michael@michaelxavier.net>
2012-06-04 22:59:28 -0700

    Remove extraneous Main

--- Main.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE OverloadedStrings #-}
-module Main (main) where
-
-import Data.Attoparsec.Text
-import System.Cron.Parser
-
-main :: IO ()
-main = mapM_ (parseTest cronSchedule) strs
-  where strs = ["* * * * 13-7"]
