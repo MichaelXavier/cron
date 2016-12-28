0608d5b43c838add56ae6cd953faefdf0aa67a91
caneroj1 <caneroj1@tcnj.edu>
2016-12-28 10:40:58 -0500

    refactoring

--- src/System/Cron/Display.hs
+++ src/System/Cron/Display.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE ViewPatterns #-}
 
 module System.Cron.Display
 (
   Verbosity(..)
 , displaySchedule
+, describeTime
 ) where
 
 import System.Cron.Types
@@ -11,6 +13,69 @@ import Data.Char           (toUpper)
 import Data.List.NonEmpty  (NonEmpty (..))
 import qualified Data.List.NonEmpty as N hiding (NonEmpty (..))
 import Data.List           (intercalate, dropWhileEnd)
+
+leftPad :: Int -> String
+leftPad n = if n < 10 then "0" ++ show n else show n
+
+viewSF :: CronField -> Maybe SpecificField
+viewSF (Field (SpecificField' s)) = Just s
+viewSF _                          = Nothing
+
+viewRD :: CronField -> Maybe RangeField
+viewRD (Field (RangeField' r)) = Just r
+viewRD _                       = Nothing
+
+viewL :: CronField -> Maybe (NonEmpty BaseField)
+viewL (ListField l) = Just l
+viewL _             = Nothing
+
+-- hasNoSpecialChars :: CronField -> Bool
+-- hasNoSpecialChars (Field (SpecificField' _)) = True
+-- hasNoSpecialChars _                          = False
+--
+-- isRangeDescriptor :: CronField -> Bool
+-- isRangeDescriptor (Field (RangeField' _)) = True
+-- isRangeDescriptor _                       = False
+--
+-- isListDescriptor :: CronField -> Bool
+-- isListDescriptor (ListField _) = True
+-- isListDescriptor _             = False
+
+describeTime :: MinuteSpec -> HourSpec -> String
+describeTime (viewSF . minuteSpec -> Just m) (viewSF . hourSpec -> Just h) = "at " ++ formatTime m h
+describeTime (viewRD . minuteSpec -> Just m) (viewSF . hourSpec -> Just h) = "every minute between " ++ time (rfBegin m) h ++ " and " ++ time (rfEnd m) h
+describeTime (viewSF . minuteSpec -> Just m) (viewL . hourSpec -> Just h)  = describeMultHours m h
+describeTime _ _ = undefined
+-- describeTime mins hours
+--   | hasNoSpecialChars minCF && hasNoSpecialChars hourCF = undefined--mkTime minCF hourCF
+--   | isRangeDescriptor minCF && hasNoSpecialChars hourCF = undefined
+--   | hasNoSpecialChars minCF && isListDescriptor  hourCF = undefined
+--   | otherwise                                           = undefined --describe minCF hourCF
+  -- where
+  --   minCF  = minuteSpec mins
+  --   hourCF = hourSpec hours
+    -- describeFields (Field (SpecificField' mf)) (Field (SpecificField' hf)) =
+    --   formatTime mf hf
+
+describeMultHours :: SpecificField -> NonEmpty BaseField -> String
+describeMultHours minuteSF (h :| hs) = either id (("at " ++) . intercalate ", ") $ describe (h : hs)
+  -- | hasStar (h : hs) = "at " ++ specificField minuteSf ++ " minutes past the hour, every hour"
+  -- | otherwise
+  where -- hasStar = elem (\Star -> True)
+    describe []       = Right []
+    describe (Star:_) = Left $ "at " ++ show (specificField minuteSF) ++ " minutes past the hour, every hour"
+    describe (o:os)   = Right . (:) (formatBaseField o) =<< describe os
+      where formatBaseField (SpecificField' s) = formatTime minuteSF s
+            formatBaseField (RangeField' r)    = show (specificField minuteSF) ++ " minutes past the hour, between " ++ show (rfBegin r) ++ " and " ++ show (rfEnd r)
+    -- describe ((SpecificField' s):hs) = describe =<< describe hs
+    -- describe ((RangeField' r):hs)    = describe =<< describe hs
+
+
+time :: Int -> SpecificField -> String
+time minute' hourF = leftPad (specificField hourF) ++ ":" ++ leftPad minute'
+
+formatTime :: SpecificField -> SpecificField -> String
+formatTime minuteF hourF = time (specificField minuteF) hourF
 
 newtype Placeholder = Placeholder {
     getPlaceholder :: String
@@ -92,7 +157,7 @@ data Verbosity = Verbose | NonVerbose
 -- human readable string
 displaySchedule :: Verbosity -> CronSchedule -> String
 displaySchedule v cs =
-  format . intercalate ", " . getList . N.map unwrapDisplay . verbosify v $
+  cap . intercalate ", " . getList . N.map unwrapDisplay . verbosify v $
       hide (minute cs) :|
     [
       hide $ hour cs
@@ -103,8 +168,8 @@ displaySchedule v cs =
   where
     getList (x :| xs) = x : xs
 
-    format []         = []
-    format (x : xs)   = toUpper x : xs
+    cap []         = []
+    cap (x : xs)   = toUpper x : xs
 
     verbosify NonVerbose (c :| cfs) = c :| dropWhileEnd unwrapIsStar cfs
     verbosify Verbose    cfs        = cfs
