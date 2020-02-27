c426ed9cab4f58689c2544730fee98be9290bfdc
Julien Debon <julien.debon@decathlon.com>
2020-02-27 11:37:49 +0100

    Fix the code
    Closes #41

--- src/System/Cron/Internal/Schedule.hs
+++ src/System/Cron/Internal/Schedule.hs
@@ -1,26 +1,20 @@
 {-# LANGUAGE CPP #-}
 
-module System.Cron.Internal.Schedule where
+module System.Cron.Internal.Schedule (findNextMinuteDelay') where
 
 import           Data.Time
-
 
 findNextMinuteDelay' :: UTCTime -> (UTCTime, Int)
 findNextMinuteDelay' now = (next, delay)
   where
-    f     = formatTime defaultTimeLocale fmtFront now
-    m     = (read (formatTime defaultTimeLocale fmtMinutes now) :: Int) + 1
-    r     = f ++ ":" ++ if length (show m) == 1 then "0" ++ show m else show m
-    next  = readTime' defaultTimeLocale fmtRead r :: UTCTime
-    diff  = diffUTCTime next now
+    oneMinuteLater = addUTCTime oneMinute now
+    plainMinute = truncateToPlainMinute $ utctDayTime oneMinuteLater
+    next = oneMinuteLater { utctDayTime = plainMinute }
+    diff = diffUTCTime next now
     delay = round (realToFrac (diff * 1000000) :: Double) :: Int
-    fmtFront   = "%F %H"
-    fmtMinutes = "%M"
-    fmtRead    = "%F %H:%M"
 
-readTime' :: TimeLocale -> String -> String -> UTCTime
-#if MIN_VERSION_time(1,5,0)
-readTime' =  parseTimeOrError True
-#else
-readTime' = readTime
-#endif
+oneMinute :: NominalDiffTime
+oneMinute = 60
+
+truncateToPlainMinute :: DiffTime -> DiffTime
+truncateToPlainMinute = fromIntegral . (* 60) . (`quot` 60) . truncate
